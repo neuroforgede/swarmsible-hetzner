8c840c0db7f3f01aca69a56fb53fdff0719b2e19
Martin Braun <braun@neuroforge.de>
2022-10-23 16:45:50 +0200

    pin more dependencies

--- swarmsible-hetzner/setup.sh
+++ swarmsible-hetzner/setup.sh
@@ -3,5 +3,5 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip3 install ansible==6.5.0
-pip3 install hcloud
-ansible-galaxy collection install hetzner.hcloud
+pip3 install hcloud==1.18.0
+ansible-galaxy collection install hetzner.hcloud:==1.8.2
