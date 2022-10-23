a976ac4471bb328d2d4ba2a02404595cd80a54b7
Martin Braun <braun@neuroforge.de>
2022-10-23 17:33:22 +0200

    wip on making roles work on empty project (load balancer is slightly buggy

--- swarmsible-hetzner/setup.sh
+++ swarmsible-hetzner/setup.sh
@@ -1,7 +1,6 @@
-rm -rf venv
-python3 -m venv venv
 source venv/bin/activate
 
-pip3 install ansible==6.5.0
-pip3 install hcloud==1.18.0
-ansible-galaxy collection install hetzner.hcloud:==1.8.2
+set -a
+export HCLOUD_TOKEN=$(yq -r .hcloud_token vars/hcloud_token.yml)
+
+ansible-playbook -i inventories/sample/ setup.yml --extra-vars='{ "swarmsible_vars_path": "vars" }'
