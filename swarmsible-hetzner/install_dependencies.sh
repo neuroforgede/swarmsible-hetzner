6e94e07fb5f8b9a77a7fe9283c57cddd0fd0ac7a
Martin Braun <braun@neuroforge.de>
2023-02-19 23:25:58 +0100

    Update install_dependencies.sh

--- swarmsible-hetzner/install_dependencies.sh
+++ swarmsible-hetzner/install_dependencies.sh
@@ -5,4 +5,4 @@ source venv/bin/activate
 pip3 install ansible==6.5.0
 pip3 install hcloud==1.18.0
 pip3 install yq
-ansible-galaxy collection install hetzner.hcloud:==1.8.2
+ansible-galaxy collection install hetzner.hcloud:==1.9.1
